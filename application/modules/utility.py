9ffcdef9ccb70dc136062a22226213f49c11a20c
mlemasso <mlemasso@gmail.com>
2014-03-08 14:12:39 +0100

    prémice du squelette de l'application

---
+++ application/modules/utility.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+""" Few utility methods """
+import os
+
+
+def match(arch):
+    """ Match additional ARCH setting """
+    arch_list = list([["i386", "x86"],
+        ["x86_64", "x86"], ["sparc32", "sparc"],
+        ["sparc64", "sparc"], ["sh64", "sh"],
+        ["tilepro", "tile"], ["tilegx", "tile"]])
+
+    for arch_iter in arch_list:
+        if arch == arch_iter[0]:
+            os.environ["SRCARCH"] = arch_iter[1]
+            return
+
+    os.environ["SRCARCH"] = os.environ.get("ARCH")
