b32e91bdef9f31d995a4bf838d1e1d21b5b6b156
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2014-03-10 10:15:36 +0100

    Nouvelle fonction dans utility, permet d'initialiser la librairie kconfiglib, TODO: récupérer dans makefile la version du noyau.

--- application/modules/utility.py
+++ application/modules/utility.py
@@ -19,3 +19,26 @@ def match(arch):
             return
 
     os.environ["SRCARCH"] = os.environ.get("ARCH")
+
+
+def init(path=".", arch="x86_64"):
+    """ Initialize environnement """
+    # Configuration de l'environnement
+    # Architecture
+    os.environ["ARCH"] = arch
+    match(arch)
+
+    # Version du noyau
+    version = "3"
+    patchlevel = "13"
+    sublevel = "0"
+    extraversion = ""
+
+    os.environ["srctree"] = path
+
+    os.environ["VERSION"] = version
+    os.environ["PATCHLEVEL"] = patchlevel
+    os.environ["SUBLEVEL"] = sublevel
+    os.environ["EXTRAVERSION"] = extraversion
+
+    os.environ["KERNELVERSION"] = version + "." + patchlevel + "." + sublevel
