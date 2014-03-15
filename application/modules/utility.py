bca325dabc95bbcc2f48aa6d41ad9f2065a15f41
JordanAupetit <aupetit.jordan@gmail.com>
2014-03-15 13:54:35 +0100

    Ajout gestion architecture -- modification fenetre d'alerte lorsque Next est clique

--- application/modules/utility.py
+++ application/modules/utility.py
@@ -6,27 +6,29 @@
 import os
 import re
 
-def match(arch):
-    """ Match additional ARCH setting """
-    arch_list = list([["i386", "x86"],
-        ["x86_64", "x86"], ["sparc32", "sparc"],
-        ["sparc64", "sparc"], ["sh64", "sh"],
-        ["tilepro", "tile"], ["tilegx", "tile"]])
+# def match(arch):
+#     """ Match additional ARCH setting """
+#     arch_list = list([["i386", "x86"],
+#         ["x86_64", "x86"], ["sparc32", "sparc"],
+#         ["sparc64", "sparc"], ["sh64", "sh"],
+#         ["tilepro", "tile"], ["tilegx", "tile"]])
 
-    for arch_iter in arch_list:
-        if arch == arch_iter[0]:
-            os.environ["SRCARCH"] = arch_iter[1]
-            return
+#     for arch_iter in arch_list:
+#         if arch == arch_iter[0]:
+#             os.environ["SRCARCH"] = arch_iter[1]
+#             return
 
-    os.environ["SRCARCH"] = os.environ.get("ARCH")
+#     os.environ["SRCARCH"] = os.environ.get("ARCH")
 
 
-def init_environ(path=".", arch="x86_64"):
+def init_environ(path=".", arch="x86_64", srcarch="", srcdefconfig=""):
     """ Initialize environnement """
     # Configuration de l'environnement
     # Architecture
     os.environ["ARCH"] = arch
-    match(arch)
+    os.environ["SRCARCH"] = srcarch
+    os.environ["SRCDEFCONFIG"] = srcdefconfig
+    # match(arch)
 
     path_copy = path
     
