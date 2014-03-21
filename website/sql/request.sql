46e6f377202a662dc426b078322179e1f113d88e
oxyasis <oxyasis@phoenix.(none)>
2014-03-21 19:30:19 +0100

    ajout de requete pour la modification de tuple hardware - option

--- website/sql/request.sql
+++ website/sql/request.sql
@@ -9,14 +9,14 @@ SELECT `option`.`id` AS `id`, `option`.`name` AS `name`,
        `kernel_version`, `kernel_sub`
 FROM `option` JOIN `hardware_option` ON (`option_id` = `option`.`id`)
      	      JOIN `hardware` ON (`hardware_id` = `hardware`.`id`)
-WHERE `hardware`.`name` LIKE '%nom_du_materiel%'
+WHERE `hardware`.`name` LIKE '%nom_du_materiel%';
 
 -- recherche de la liste des matériels pour une option donnée
 SELECT `hardware`.`id` AS `id`, `hardware`.`name` AS `name`, 
        `kernel_version`, `kernel_sub`
 FROM `option` JOIN `hardware_option` ON (`option_id` = `option`.`id`)
      	      JOIN `hardware` ON (`hardware_id` = `hardware`.`id`)
-WHERE `option`.`name` LIKE '%nom_de_l_option%'
+WHERE `option`.`name` LIKE '%nom_de_l_option%';
 
 -- ajout d'un tuple materiel - option
 -- on verifie que le materiel et / ou l'option n'existent pas déjà
@@ -24,30 +24,50 @@ WHERE `option`.`name` LIKE '%nom_de_l_option%'
 --    l'utilisateur cherche à ajouter n'est pas dans la liste retournée
 --    si oui, il faudra mofifier la chaine entrée par l'utilisateur par 
 --    celle qu'il veut dans la liste
-SELECT * FROM `hardware` WHERE `name` LIKE '%nom_du_materiel%' ORDER BY `name`
-SELECT * FROM `option` WHERE `name` LIKE '%nom_de_l_option%' ORDER BY `name`
+SELECT * FROM `hardware` WHERE `name` LIKE '%nom_du_materiel%' ORDER BY `name`;
+SELECT * FROM `option` WHERE `name` LIKE '%nom_de_l_option%' ORDER BY `name`;
 
 -- sinon, on ajoute
-INSERT INTO `hardware` (`name`) VALUES ('nom_du_materiel')
-INSERT INTO `option` (`name`) VALUES ('nom_de_l_option')
+INSERT INTO `hardware` (`name`) VALUES ('nom_du_materiel');
+INSERT INTO `option` (`name`) VALUES ('nom_de_l_option');
 
 -- et si on garde les id en auto increment / les tables hardware - option
 -- il faut récupérer l'id qu'on a pas explicitement dans l'insertion d'avant,
 -- pour ensuite faire l'insertion dans la table hardware_option
-SELECT `id` AS `hardware_id` FROM `hardware` WHERE `name` = 'nom_du_materiel'
-SELECT `id` AS `option_id` FROM `option` WHERE `name` = 'nom_de_l_option'
+SELECT `id` AS `hardware_id` FROM `hardware` WHERE `name` = 'nom_du_materiel';
+SELECT `id` AS `option_id` FROM `option` WHERE `name` = 'nom_de_l_option';
 
 INSERT INTO `hardware_option` (`hardware_id`, `option_id`) 
-       VALUES (id_du_materiel, id_de_l_option)
+       VALUES (id_du_materiel, id_de_l_option);
 
 -- modifier / supprimer un materiel
-UPDATE `hardware` SET `name`='nouveau_nom_materiel' WHERE `id` = id_du_materiel
-DELETE FROM `hardware_option` WHERE `id` = id_du_materiel
-DELETE FROM `hardware` WHERE `id` = id_du_materiel
+UPDATE `hardware` SET `name`='nouveau_nom_materiel' WHERE `id` = id_du_materiel;
+DELETE FROM `hardware_option` WHERE `id` = id_du_materiel;
+DELETE FROM `hardware` WHERE `id` = id_du_materiel;
 
 -- modifier / supprimer une option
-UPDATE `option` SET `name`='nouveau_nom_option' WHERE `id` = id_de_l_option
-DELETE FROM `hardware_option` WHERE `id` = id_de_l_option
-DELETE FROM `option` WHERE `id` = id_de_l_option
+UPDATE `option` SET `name`='nouveau_nom_option', 
+       		    `kernel_version`='nouveau_kernel', 
+		    `kernel_sub`='nouveau_sub' 
+WHERE `id` = id_de_l_option;
+DELETE FROM `hardware_option` WHERE `id` = id_de_l_option;
+DELETE FROM `option` WHERE `id` = id_de_l_option;
 
--- modifier un tuple en fonction du materiel
+-- recherche de tuples à modifier
+-- présence de doublons en cas de recherche peu précise
+-- TODO à voir pour corriger
+SELECT `option_id`, `hardware_id`, `option`.`name`, `hardware`.`name` 
+       `kernel_version`, `kernel_sub`;
+FROM `option` JOIN `hardware_option` ON (`option_id` = `option`.`id`)
+     	      JOIN `hardware` ON (`hardware_id` = `hardware`.`id`);
+WHERE `hardware`.`name` LIKE '%nom_du_materiel%' OR 
+      `option`.`name` LIKE '%nom_de_l_option';
+
+-- modification d'un tuple
+UPDATE `hardware_option` SET `hardware_id`='nouvel_id_materiel',
+       			     `option_id`='nouvel_id_option'
+WHERE `hardware_id` = id_du_materiel AND `option_id` = id_de_l_option;
+
+-- suppression d'un tuple
+DELETE FROM `hardware_option` 
+WHERE `hardware_id` = id_du_materiel AND `option_id` = id_de_l_option;
